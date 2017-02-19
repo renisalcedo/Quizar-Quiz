fe6ceadc085ccacbb5d70ea11352557fdd8799d2
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-02-19 12:08:29 -0500

    Added max score

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,8 +15,7 @@ var quizText;
 // Question for Quiz
 var quest = ["Is the Nucleus the brain of the Cell?", "Is the Mitochondria the powerhouse of the cell?",
             "Does the Mitochondria stores food, water, and waste in the cell?", "Does Ribosome turns amino acids into proteins?",
-            "Does Endoplasmic Reticulum transports materials throughout the cell?"];  
- 
+            "Does Endoplasmic Reticulum transports materials throughout the cell?"];
 
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update});
 
@@ -34,7 +33,6 @@ function preload() {
   game.load.image('play', 'assets/img/play.png');
   game.load.image('true', 'assets/img/true-button.png');
   game.load.image('false', 'assets/img/false-button.png');
-  
 }
 
 // empty cannons
@@ -47,7 +45,7 @@ var questions;
 var score = 0;
 var textScore;
 var can_shoot = true;
-    
+
 // Enemy Values
 var enemy;
 var enemies;
@@ -59,33 +57,33 @@ function create() {
   //Music name: Jim Yosef - Firefly NCS Release
   music = game.add.audio('music');
 	music.play();
-	
+
   // Maps
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.add.sprite(0, 0, 'space-map');
-  
+
   // Enemy
   enemies = game.add.group();
   enemies.enableBody = true;
   enemies.physicsBodyType = Phaser.Physics.ARCADE;
-  
+
   // Objects
   questions = game.add.group();
   weapon = game.add.weapon(1, 'cannon');
-  
+
   // Turrets
   weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
   weapon.bulletSpeed = 200;
   weapon.fireRate = 3;
-  
+
   sprite = this.add.sprite(400, 300, 'ship');
   sprite.anchor.set(0.5);
   game.physics.arcade.enable(sprite);
-   
+
   weapon.trackSprite(sprite, 0, 0, true);
   // Initial Functionalities
   gameIntro();
-  
+
 
   // Adds the key spacebar
   game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -93,31 +91,26 @@ function create() {
 
 // Updater
 var i = 0;
-
 function update() {
   if(playing == true) {
-    
+
     game.physics.arcade.overlap(weapon.bullets, enemies, collisionHandler, null, this);
-    
+
     // Shoots a cannon everytime the space keyboard is down
     if(game.time.now - cannonTime > 250) {
       cannonTime = game.time.now;
     }
-    
+
     if(game.time.now - enemyTime > 1350) {
       releaseEnemy();
       enemyTime = game.time.now;
     }
-    
+
     // Quiz Sections
-     
     if(!questioning && i < quest.length) {
       getQuiz(quest[i]);
       getAnswer();
       i++;
-    }
-    else if(i > quest.length) {
-      game.state.restart();
     }
   }
 }
@@ -126,7 +119,7 @@ function releaseEnemy() {
   enemy = enemies.getFirstExists(false);
   const enemyX = enemy.x+605;
   const enemyY = enemy.y-300;
-  
+
   if(enemy) {
     enemy.reset(enemyX, enemyY-100); // Generate Enemy at position
     enemy.body.velocity.x = -85; // Sets the velocity of the Enemy
@@ -135,7 +128,7 @@ function releaseEnemy() {
 
 function gameIntro() {
   console.log("introducing")
-  
+
   button = game.add.button(game.world.centerX - 105, 495, 'play', initGame, this, 2, 1, 0);
 
   var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
@@ -148,7 +141,6 @@ function gameIntro() {
 
   text.addColor('#ff00ff', 28);
   text.addColor('#ffffff', 32);
-  
 
   console.log("introduced")
 }
@@ -157,7 +149,7 @@ function initGame() {
   button.kill();
   text.kill();
   playing = true;
-  
+
   // Quiz initialization
   var qStyle = { font: "35px Arial", fill: "#eee", align: "center" };
   quizText = game.add.text(game.world.centerX, game.world.centerY+185, quest[0], qStyle);
@@ -172,7 +164,6 @@ function initGame() {
     // Checks for walls
     b.checkWorldBounds = true;
   }
-  
   // Creates Score
   var style = {font: "65px Arial", fill: "#fff", align: "left"};
   var scur = "Score: "+score;
@@ -181,7 +172,7 @@ function initGame() {
 
 function getScore() {
   textScore.setText("Score: " + score);
-} 
+}
 
 function collisionHandler (bullet, enemies) {
   bullet.kill();
@@ -190,7 +181,6 @@ function collisionHandler (bullet, enemies) {
 }
 
 function getQuiz(quest) {
-  
   quizText.anchor.set(0.5);
   // Updates Question to current
   quizText.setText(quest);
@@ -203,9 +193,14 @@ function getAnswer() {
 }
 
 function rAns() {
-  weapon.fire();
-  score+=10 ;
-  questioning = false;
+  if(score > 50) {
+    game.state.restart();
+  }
+  else {
+    weapon.fire();
+    score+=10 ;
+    questioning = false;
+  }
 }
 
 function wAns(){
